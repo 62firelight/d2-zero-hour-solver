f4a3c131e8f1cc04a2d847e05db7ab85952750fc
Luke Tang <tanlu824@student.otago.ac.nz>
2023-03-24 13:35:05 +1300

    Change configuration buttons to coloured buttons

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,11 +1,30 @@
 import React from "react";
 
+const ELEMENT_COLORS = new Map([
+    ["Void", "purple"],
+    ["Solar", "orange"],
+    ["Arc", "lightblue"],
+]);
+
 function FilterButton(props) {
-    const buttonName = props.isPressed ? <strong>{props.element}</strong> : <div>{props.element}</div>;
+    // const buttonName = props.isPressed ? (
+    //     <strong>{props.element}</strong>
+    // ) : (
+    //     <div>{props.element}</div>
+    // );
 
     return (
-        <button onClick={() => props.toggleElement(props.element)}>
-            {buttonName}
+        <button
+            className="filter-button"
+            style={{
+                backgroundColor: ELEMENT_COLORS.get(props.element),
+                border: props.isPressed
+                    ? "8px dotted"
+                    : "8px solid transparent",
+            }}
+            title={props.element}
+            onClick={() => props.toggleElement(props.element)}
+        >
         </button>
     );
 }
